08f36055a705986c08d80822c091a23bc05c01e1
Zobachov Vladimir <zobachov@gmail.com>
2023-09-20 12:43:33 +0500

    feat: add realize method GetData in AppAPI interface

--- internal/proxi/applications/app.go
+++ internal/proxi/applications/app.go
@@ -1,3 +1,35 @@
 package applications
 
+import (
+	"io/ioutil"
+	"net/http"
+)
+
 type AppAPI struct{}
+
+func (a *AppAPI) GetData(w http.ResponseWriter, r *http.Request, accessToken string) (data []byte, err error) {
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", "https://sad.oshs.mvd.ru/api", nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	req.Header.Set("Authorization", "Bearer "+accessToken) // Set the appKey in the request header
+
+	resp, err := client.Do(req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	return []byte(body), err
+}
