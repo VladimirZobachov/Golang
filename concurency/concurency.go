812a0023ea399416d7618258070d5e4094341db7
ildus <2234944@gmail.com>
2023-10-24 10:42:06 +0500

    lesson 5

---
+++ concurency/concurency.go
@@ -0,0 +1,28 @@
+package concurency
+
+/*
+что такое горутина и чем отличается от потока
+как создать рутину и как управлять их ожиданием
+что такое канал, типы каналов
+блокировка и дедлок
+что можно делать с каналами
+проверка на закрытость и zero values
+примитивы синхронизации (Mutex, WaitGroup, Atomic, channel)
+*/
+
+/*
+go-tour!! в первую очередь
+
+https://habr.com/ru/articles/141853/
+https://golangify.com/goroutines
+https://medium.com/german-gorelkin/goroutine-leaks-bcf3bbb59997 здесь 5 статей - изучить все
+	Race condition и Data Race;
+	Deadlocks, Livelocks и Starvation;
+	Примитивы синхронизации в Go
+	Безопасная работа с каналами в Go;
+	Goroutine Leaks
+
+https://golang-blog.blogspot.com/2020/05/sync-mutex-or-channel.html коротко о том что и где использовать
+
+
+*/
